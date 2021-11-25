08aa82041e02b3fbf2316d64f218df4136269f9f
jump99500 <jump99500@naver.com>
2021-11-25 17:40:31 +0900

    step2

--- 05_igass.tf
+++ 05_igass.tf
@@ -1,9 +1,9 @@
-resource "aws_route_table_association" "ssw_igas_puba" {
+resource "aws_route_table_association" "ssw_igass_puba" {
     subnet_id = aws_subnet.ssw_puba.id
     route_table_id = aws_route_table.ssw_rt.id
 }
 
-resource "aws_route_table_association" "ssw_igas_pubc" {
+resource "aws_route_table_association" "ssw_igass_pubc" {
     subnet_id = aws_subnet.ssw_pubc.id
     route_table_id = aws_route_table.ssw_rt.id
 }
